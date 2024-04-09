d6a6f0bfa9fc69c13dc534199fab1f82d2a9de94
sweerasingha <sachiprojects@gmail.com>
2024-04-09 20:33:39 +0530

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', [HomeController::class, 'index'])->name('dashboard');
+
+require __DIR__.'/auth.php';
